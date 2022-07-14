81abb5cd2b080df9c4c7c2e56efc3ed389a8bf7e
Kenneth Österholm <kenneth.osterholm@noaignite.com>
2022-07-15 00:14:52 +0200

    t1§feat: add preview

---
+++ sanity/src/utils/resolvePreviewUrl.js
@@ -0,0 +1,16 @@
+const previewSecret = '@kringlan'
+
+const remoteUrl = process.env.SANITY_STUDIO_REMOTE_APP_URL
+const localUrl = `http://localhost:3000`
+
+export default function resolveProductionUrl(doc) {
+  const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
+
+  const previewUrl = new URL(baseUrl)
+
+  previewUrl.pathname = `/api/preview`
+  previewUrl.searchParams.append(`secret`, previewSecret)
+  previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`)
+
+  return previewUrl.toString()
+}
--- src/api/sanity/sanityClient.js
+++ src/api/sanity/sanityClient.js
@@ -1,11 +1,27 @@
-import sanityClient from '@sanity/client'
+import createClient from '@sanity/client'
+import { createPreviewSubscriptionHook } from 'next-sanity'
 
-const defaultClient = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION,
-  token: process.env.SANITY_TOKEN,
+export const config = {
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2021-10-21',
   useCdn: process.env.NODE_ENV !== 'development',
+}
+
+// Set up the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config)
+
+// Set up a preview client with serverless authentication for drafts
+export const previewClient = createClient({
+  ...config,
+  token: process.env.SANITY_TOKEN,
+  useCdn: false,
 })
 
-export default defaultClient
+export const usePreviewSubscription = createPreviewSubscriptionHook({
+  ...config,
+  useCdn: false,
+})
+
+// Helper function for easily switching between normal client and preview client
+export const getClient = (usePreview) => (usePreview ? previewClient : sanityClient)
